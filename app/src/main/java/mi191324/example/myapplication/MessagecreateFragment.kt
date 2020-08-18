afe990487abed3570bd251cab6fcde985fcba2ca
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2020-08-18 21:53:20 +0900

    fragment_messageにアクションバー追加

--- app/src/main/java/mi191324/example/myapplication/MessagecreateFragment.kt
+++ app/src/main/java/mi191324/example/myapplication/MessagecreateFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 /**
  * A simple [Fragment] subclass.
  * Use the [MessagecreateFragment.newInstance] factory method to
@@ -28,8 +29,10 @@ class MessagecreateFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messagecreate, container, false)
     }
@@ -44,12 +47,13 @@ class MessagecreateFragment : Fragment() {
          * @return A new instance of fragment MessagecreateFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                MessagecreateFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MessagecreateFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }
