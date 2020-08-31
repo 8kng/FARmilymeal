03054647c538e4c0a75ab9b512a1bacdd9c4c4bf
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2020-08-31 17:30:37 +0900

    エラー

--- app/src/main/java/mi191324/example/myapplication/MessagecreateFragment.kt
+++ app/src/main/java/mi191324/example/myapplication/MessagecreateFragment.kt
@@ -1,15 +1,21 @@
 package mi191324.example.myapplication
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_messagecreate.*
 import java.io.File
+import java.io.IOException
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -26,11 +32,13 @@ class MessagecreateFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    lateinit var mp: MediaPlayer
-    lateinit var rec: MediaRecorder
-    lateinit var fl: File
-    val filePath: String = android.os.Environment.getExternalStorageDirectory().toString() + "/sample.wav"
-
+    private var fileName: String = ""
+    private var recordButton: RecordButton? = null
+    private var recorder: MediaRecorder? = null
+    private var playButton: PlayButton? = null
+    private var player: MediaPlayer? = null
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,40 +48,116 @@ class MessagecreateFragment : Fragment() {
         }
     }
 
-    private fun startRecord() {
-        var wavFile: File = File(filePath)
-        if (wavFile.exists()){
-            wavFile.delete()
-        }
-        try {
-            rec = MediaRecorder()
-            rec.setAudioSource(MediaRecorder.AudioSource.MIC)
-            rec.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
-            rec.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
-            rec.setOutputFile(filePath)
-            rec.prepare()
-            rec.start()
-        } catch (e: Exception) {
-            e.printStackTrace()
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
+    }
+
+    private fun startPlaying(LOG_TAG: String?) {
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
         }
     }
 
-    private fun stopRecord() {
-        try {
-            rec.stop()
-            rec.reset()
-            rec.release()
-        } catch (e: java.lang.Exception) {
-            e.printStackTrace()
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
         }
     }
 
-    private fun choose(){
-        if(recordingBtn.isChecked == true){
-            startRecord()
-        } else{
-            stopRecord()
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
         }
+        recorder = null
+    }
+
+    internal inner class RecordButton(ctx: Context) : Button(ctx) {
+
+        var mStartRecording = true
+
+        var clicker: OnClickListener = OnClickListener {
+            onRecord(mStartRecording)
+            text = when (mStartRecording) {
+                true -> "Stop recording"
+                false -> "Start recording"
+            }
+            mStartRecording = !mStartRecording
+        }
+
+        init {
+            text = "Start recording"
+            setOnClickListener(clicker)
+        }
+    }
+
+    internal inner class PlayButton(ctx: Context) : Button(ctx) {
+        var mStartPlaying = true
+        var clicker: OnClickListener = OnClickListener {
+            onPlay(mStartPlaying)
+            text = when (mStartPlaying) {
+                true -> "Stop playing"
+                false -> "Start playing"
+            }
+            mStartPlaying = !mStartPlaying
+        }
+
+        init {
+            text = "Start playing"
+            setOnClickListener(clicker)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) getActivity()?.finish()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        recorder?.release()
+        recorder = null
+        player?.release()
+        player = null
     }
 
     override fun onCreateView(
@@ -84,9 +168,15 @@ class MessagecreateFragment : Fragment() {
         // Inflate the layout for this fragment
         val recordingBtn : ToggleButton = View.findViewById(R.id.recordingBtn)
         val messagecreateView : View = View.findViewById(R.id.messagecreateView)
+        val playerBtn : Button = View.findViewById(R.id.PlayBtn)
+        rec = MediaRecorder()
 
         recordingBtn.setOnClickListener(){
             choose()
+        }
+        playerBtn.setOnClickListener(){
+            startPlay()
+            stopPlay()
         }
         return View
     }
@@ -109,5 +199,7 @@ class MessagecreateFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
     }
 }
