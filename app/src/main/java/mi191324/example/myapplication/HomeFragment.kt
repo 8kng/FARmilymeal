896713e7d776bb5cfaed79e43b45417f8cff2791
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2020-10-29 02:01:28 +0900

    電話番号設定画面

--- app/src/main/java/mi191324/example/myapplication/HomeFragment.kt
+++ app/src/main/java/mi191324/example/myapplication/HomeFragment.kt
@@ -1,14 +1,19 @@
 package mi191324.example.myapplication
 
 import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
@@ -16,6 +21,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.text.SimpleDateFormat
@@ -39,6 +45,7 @@ class HomeFragment : Fragment() {
     private var param2: String? = null
     var photolist = arrayOfNulls<Uri>(20)
     var datelist = arrayOfNulls<DateTimeException>(20)
+    var words: String = "09097865827"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,12 +63,15 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val View =  inflater.inflate(R.layout.fragment_home, container, false)
         val recycler_view : RecyclerView = View.findViewById(R.id.recycler_me)
+        val FAB : FloatingActionButton = View.findViewById(R.id.FAB)
 
         val httpurl = "https://asia-northeast1-farmily-meal.cloudfunctions.net/photolist"
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val format = SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss")
         val day = format.format(Date())
         val calendar1 = Calendar.getInstance()
+        val pref = PreferenceManager.getDefaultSharedPreferences(context)
+        val number = pref.getString("number", "")
 
         val httpAsync = httpurl
             .httpGet()
@@ -74,22 +84,26 @@ class HomeFragment : Fragment() {
                         val res = moshi.adapter(PhotoListResponse::class.java).fromJson(data)
                         recycler_view.adapter = HomeAdpter(res!!.photos)
 
-                        val time1_be:Date = format.parse(day)
-                        val time2:Date = format.parse(res.photos[0].datetime)
+                        val time1_be: Date = format.parse(day)
+                        val time2: Date = format.parse(res.photos[0].datetime)
                         calendar1.setTime(time1_be)
                         calendar1.add(Calendar.MINUTE, 20)
                         val time1_af = calendar1.getTime()
                         Log.d("始まり", time1_be.toString())
                         Log.d("現在", time2.toString())
                         Log.d("終わり", time1_af.toString())
-                        if ((time1_be <= time2) && (time1_af >= time2)){
+                        if ((time1_be <= time2) && (time1_af >= time2)) {
                             popupWindow()
                         }
                     }
                     is Result.Failure -> {
                         val (user, err) = result
                         Log.d("No", "${user}")
-                        val myToast: Toast = Toast.makeText(getActivity(), "データの受信に失敗しました", Toast.LENGTH_LONG)
+                        val myToast: Toast = Toast.makeText(
+                            getActivity(),
+                            "データの受信に失敗しました",
+                            Toast.LENGTH_LONG
+                        )
                         myToast.show()
                     }
                 }
@@ -98,6 +112,13 @@ class HomeFragment : Fragment() {
 
         recycler_view.layoutManager = LinearLayoutManager(context)
         recycler_view.setHasFixedSize(true)
+
+        FAB.setOnClickListener{ view ->
+            phonenumber(number)
+            val myToast: Toast = Toast.makeText(getActivity(), number, Toast.LENGTH_LONG)
+            myToast.show()
+        }
+
         return View
     }
 
@@ -111,19 +132,56 @@ class HomeFragment : Fragment() {
         val photos: List<Photo>
     )
 
-    private fun popupWindow(){  /*電話をかけるかのポップアップウィンドウ表示*/
+    private fun phonenumber(number: String?){
+        val pref = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = pref.edit()
+        val myedit = EditText(requireContext())
+        var word: String = ""
+        myedit.maxLines = 1
+        val dialog = AlertDialog.Builder(requireActivity())
+        dialog.setTitle("電話番号入力画面")
+        dialog.setMessage("GoogleHomeの電話番号を設定します")
+        dialog.setView(myedit)
+        myedit.setText(number)
+        dialog.setPositiveButton("これでOK", DialogInterface.OnClickListener { dialog, which ->
+            editor.putString("number", myedit.text.toString())
+                .commit()
+            val imm = getActivity()?.getSystemService(
+                Context.INPUT_METHOD_SERVICE
+            ) as InputMethodManager
+            imm.hideSoftInputFromWindow(myedit.getWindowToken(), 0)
+            dialog.dismiss()
+        })
+        dialog.setNegativeButton("No", { dialog, which ->  /*電話しないと選択時*/
+            dialog.dismiss()
+            val imm = getActivity()?.getSystemService(
+                Context.INPUT_METHOD_SERVICE
+            ) as InputMethodManager
+            imm.hideSoftInputFromWindow(myedit.getWindowToken(), 0)
+        })
+        dialog.show()
+    }
+
+    private fun popupWindow(){/*電話をかけるかのポップアップウィンドウ表示*/
         AlertDialog.Builder(requireContext())
             .setTitle("ただいまお食事をしているようです!")
             .setMessage("Duoアプリに移動して電話を掛けますか?")
             .setPositiveButton("Yes", { dialog, which ->  /*電話をすると選択時*/
                 val intent = Intent()
                 intent.setAction("com.google.android.apps.tachyon.action.CALL")
-                intent.setClassName("com.google.android.apps.tachyon", "com.google.android.apps.tachyon.ExternalCallActivity"); /*Duoアプリに移動*/
-                intent.setData(Uri.parse("tel:07038027280"))
-                try{
+                intent.setClassName(
+                    "com.google.android.apps.tachyon",
+                    "com.google.android.apps.tachyon.ExternalCallActivity"
+                ); /*Duoアプリに移動*/
+                intent.setData(Uri.parse("tel:"))
+                try {
                     startActivity(intent)
-                } catch (e: Exception){
-                    val Toast: Toast = Toast.makeText(getActivity(), "Duoアプリが見つかりません", Toast.LENGTH_LONG) /*アプリが見つからなかった場合*/
+                } catch (e: Exception) {
+                    val Toast: Toast = Toast.makeText(
+                        getActivity(),
+                        "Duoアプリが見つかりません",
+                        Toast.LENGTH_LONG
+                    ) /*アプリが見つからなかった場合*/
                     Toast.show()
                 }
             })
